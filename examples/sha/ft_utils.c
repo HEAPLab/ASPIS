267591a01824dea29840a034ea5a988d9e283f98
davidebaroffio-polimi <davide.baroffio@mail.polimi.it>
2024-04-08 16:10:56 +0200

    Fixed sha benchmark

--- examples/sha/ft_utils.c
+++ examples/sha/ft_utils.c
@@ -1,3 +1,5 @@
+
+#include <string.h>
 #include "ft_utils.h"
 
 void DataCorruption_Handler() {
@@ -8,11 +10,11 @@ void SigMismatch_Handler() {
     while(1);
 }
 
-__attribute__((annotate("to_duplicate"))) void memcpy_to_duplicate(void *__restrict__ __dest, const void *__restrict__ __src, size_t __n) {
+__attribute__((annotate("to_duplicate"))) void memcpy_to_duplicate(void *__dest, const void *__src, size_t __n) {
     memcpy(__dest, __src, __n);
 }
 
 
-__attribute__((annotate("to_duplicate"))) void memset_to_duplicate(void *__restrict__ __dest, const int __src, size_t __n) {
+__attribute__((annotate("to_duplicate"))) void memset_to_duplicate(void *__dest, const int __src, size_t __n) {
     memset(__dest, __src, __n);
 }
