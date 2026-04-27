ae9579816f761ff8af0c59a05539d2f877f15e3a
Gab-San <santandreagabriele+github@gmail.com>
2026-04-27 10:54:01 +0200

    test: global runtime signature declaration as coverage test
    Reinstated inter-rasm in tests

---
+++ testing/tests/c/declared_signatures/global_var_across_functions.c
@@ -0,0 +1,29 @@
+/* 
+* Duplicated variable across functions.
+*/
+
+#include <stdio.h>
+
+__attribute__((annotate("to_harden")))
+int g = 0;
+
+int runtime_sig;
+int run_adj_sig;
+
+void increment() {
+    g += 1;
+}
+
+void print() {
+    printf("%d", g);
+}
+
+int main() {
+    increment();
+    increment();
+    print();
+    return 0;
+}
+
+// expected output
+// 2
